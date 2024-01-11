ee7c0c806289b14c07377d63d5408b3cb520dfcd
Daria Vdovina <vdovinadn@gmail.com>
2024-01-11 18:39:40 +0300

    build: parsers

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,9 @@ import mongoose from 'mongoose';
 const app = express();
 
 const { PORT = 3000 } = process.env;
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT);
