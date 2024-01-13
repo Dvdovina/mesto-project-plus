56deaa96dfee9af4ec265a890632eb897ce069a6
Daria Vdovina <vdovinadn@gmail.com>
2024-01-13 16:36:43 +0300

    build: libraries import/debug script/logger

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
 import mongoose from 'mongoose';
+import { celebrate, Joi } from 'celebrate';
 
 const app = express();
 
---
+++ src/middlewares/logger.ts
@@ -0,0 +1,3 @@
+import winston from 'winston';
+import expressWinston from 'express-winston';
+import 'winston-daily-rotate-file';
