7b1aba652ed18dd2580a9f00a3c3f934bb6b95bf
Daria Vdovina <vdovinadn@gmail.com>
2024-01-18 20:16:00 +0300

    build: mongoose connect mestodb

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,14 @@ import { celebrate, Joi } from 'celebrate';
 
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
 const { PORT = 3000 } = process.env;
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server is active on port ${PORT}`);
+});
