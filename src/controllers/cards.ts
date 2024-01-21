6e1dad1f2bfe2a3cbcf9e9fa92c739e58359d6f4
Daria Vdovina <vdovinadn@gmail.com>
2024-01-21 19:30:42 +0300

    build: new users/cards controllers

--- src/controllers/cards.ts
+++ src/controllers/cards.ts
@@ -1,14 +1,31 @@
 import Card from "../models/card";
 import { Request, Response } from "express";
-import { OK_STATUS, ERROR_STATUS } from "../constants";
+import { OK_STATUS, BAD_REQUEST_STATUS, NOT_FOUND_STATUS } from "../constants";
 
+
+export const getCards = (req: Request, res: Response) => {
+  Card.find({})
+    .then((cards) => res.status(OK_STATUS).send({ data: cards }))
+    .catch(() => res.status(BAD_REQUEST_STATUS).send({ message: 'Сервер не смог обработать запрос' }));
+};
 
 export const createCard = (req: Request, res: Response) => {
-  const { name, link } = req.body;
-  Card.create({ name, link, owner: (req as any).user._id })
+  const { name, link, _id } = req.body;
+  Card.create({ name, link, owner: _id })
     .then((card) => {
       res.status(OK_STATUS).send({ data: card });
     })
-    .catch((err) => res.status(ERROR_STATUS).send(err));
+    .catch(() => res.status(BAD_REQUEST_STATUS).send({ message: 'Сервер не смог обработать запрос' }));
+};
+
+export const deleteCardById = (req: Request, res: Response) => {
+  Card.deleteOne({ _id: req.params.cardId })
+    .then((card) => {
+      if (card.deletedCount === 0) {
+        res.status(NOT_FOUND_STATUS).send({ message: 'Карточка не найдена' });
+      }
+      res.status(OK_STATUS).send({ message: 'Карточка удалена' });
+    })
+    .catch(() => res.status(BAD_REQUEST_STATUS).send({ message: 'Сервер не смог обработать запрос' }));
 };
 
--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,20 +1,36 @@
 import User from "../models/user";
 import { Request, Response } from "express";
 import bcrypt from 'bcrypt';
-import { OK_STATUS, ERROR_STATUS } from "../constants";
+import { OK_STATUS, BAD_REQUEST_STATUS, NOT_FOUND_STATUS } from "../constants";
 
 
 export const getUsers = (req: Request, res: Response) => {
   User.find({})
     .then((users) => res.status(OK_STATUS).send({ data: users }))
-    .catch(() => res.status(ERROR_STATUS).send({ message: 'Сервер не смог обработать запрос' }));
+    .catch(() => res.status(BAD_REQUEST_STATUS).send({ message: 'Сервер не смог обработать запрос' }));
+};
+
+export const getUserById = (req: Request, res: Response) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (user) {
+        res.status(OK_STATUS).send({
+          name: user.name,
+          about: user.about,
+          avatar: user.avatar,
+          _id: user._id,
+        });
+      }
+      return res.status(NOT_FOUND_STATUS).send({ message: 'Пользователь не найден' });
+    })
+    .catch(() => res.status(BAD_REQUEST_STATUS).send({ message: 'Сервер не смог обработать запрос' }));
 };
 
 export const createUser = (req: Request, res: Response) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.status(OK_STATUS).send({ data: user }))
-    .catch(() => res.status(ERROR_STATUS).send({ message: 'Сервер не смог обработать запрос' }));
+    .catch(() => res.status(BAD_REQUEST_STATUS).send({ message: 'Сервер не смог обработать запрос' }));
 };
 
 
