ad8f9ddd3cef84cfd4b2036e48189abb28b69965
Daria Vdovina <vdovinadn@gmail.com>
2024-01-21 19:30:12 +0300

    build: getUserbyID route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createUser, getUsers } from "../controllers/users";
+import { createUser, getUsers, getUserById } from "../controllers/users";
 
 const userRouter = Router();
 
 
 userRouter.get('/users', getUsers);
+userRouter.get('/users/:userId', getUserById);
 userRouter.post('/users', createUser);
 
 
