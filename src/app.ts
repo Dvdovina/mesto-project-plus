b219ce7aa80a8bce7382d2ccf2d3a5fd17181c5b
Daria Vdovina <vdovinadn@gmail.com>
2024-01-21 19:29:03 +0300

    build: test user

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,14 @@ const app = express();
 
 const { PORT = 3000 } = process.env;
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+  req.body = {
+    ...req.body,
+    _id: "65ad3fe7dcc0838fe296b657",
+  };
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -16,6 +24,7 @@ app.use(cardRouter);
 
 
 const connect = async() => {
+  mongoose.set('strictQuery', true);
   await mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
   console.log("Подключились к базе данных")
   await app.listen(PORT)
